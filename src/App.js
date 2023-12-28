2d4f98f3584db2fc02a12f18108f78e8fe98d249
fsdevandrii <135020138+fsdevandrii@users.noreply.github.com>
2023-12-28 12:44:56 +0200

    app-onclick

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from "react";
+import "./App.css";
 
 function App() {
+  const list = [
+    { id: 1, text: "Перший елемент", link: "/first" },
+    { id: 2, text: "Другий елемент", link: "/second" },
+    { id: 3, text: "Третій елемент", link: "/last" },
+  ];
+
+  const handleLinkClick = () => {
+    return window.confirm("Вийти?");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {list.map((item) => (
+          <Fragment key={item.id}>
+            <Link {...item} />
+          </Fragment>
+        ))}
+
+        <Link
+          text="My link"
+          link="www.gogle.com"
+          getConfirm={handleLinkClick}
+        />
+
+        <Input />
       </header>
     </div>
   );
+}
+
+function Link({ text, link, getConfirm }) {
+  const handleClick = (e) => {
+    console.log(e.target);
+
+    const result = getConfirm();
+
+    if (result) {
+      window.location.assign(link);
+    }
+  };
+
+  return (
+    <span className="App-link" onClick={handleClick}>
+      {text}
+    </span>
+  );
+}
+
+function Input() {
+  const handleInput = (e) => {
+    console.log(e.target.value);
+  };
+
+  return <Input onChange={handleInput} />;
 }
 
 export default App;
